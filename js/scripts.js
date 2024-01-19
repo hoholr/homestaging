d5a73808ca1baa7878cd5146b413e5256f7e0a47
hoholroot <128160350+hoholr@users.noreply.github.com>
2024-01-19 14:56:07 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
     });
 
 });
+
+    if ('serviceWorker' in navigator) {
+       navigator.serviceWorker.register("serviceworker.js");
+    }
+    
+    const urlsToCache = ["/"];
+    self.addEventListener("install", (event) => {
+       event.waitUntil(async () => {
+          const cache = await caches.open("pwa-assets");
+          return cache.addAll(urlsToCache);
+       });
+    });
